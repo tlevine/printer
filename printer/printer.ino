6a1e8a4196832aab21846274f6d42b3282028899
Thomas Levine <occurrence@thomaslevine.com>
2013-02-18 17:00:35 +0000

    Print whatever is sent to the Arduino serial

---
+++ printer/printer.ino
@@ -0,0 +1,52 @@
+/*************************************************************************
+  This is an Arduino library for the Adafruit Thermal Printer.
+  Pick one up at --> http://www.adafruit.com/products/597
+  These printers use TTL serial to communicate, 2 pins are required.
+
+  Adafruit invests time and resources providing this open source code.
+  Please support Adafruit and open-source hardware by purchasing products
+  from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.
+  MIT license, all text above must be included in any redistribution.
+ *************************************************************************/
+
+// If you're using Arduino 1.0 uncomment the next line:
+#include "SoftwareSerial.h"
+// If you're using Arduino 23 or earlier, uncomment the next line:
+//#include "NewSoftSerial.h"
+
+#include "Adafruit_Thermal.h"
+#include "adalogo.h"
+#include "adaqrcode.h"
+#include <avr/pgmspace.h>
+
+int printer_RX_Pin = 5;  // This is the green wire
+int printer_TX_Pin = 6;  // This is the yellow wire
+
+Adafruit_Thermal printer(printer_RX_Pin, printer_TX_Pin);
+
+void setup(){
+  Serial.begin(9600);
+  pinMode(7, OUTPUT); digitalWrite(7, LOW); // To also work w/IoTP printer
+  printer.begin();
+}
+
+int i;
+char thisLine[30];
+
+void loop() {
+  while (Serial.available() > 0) {
+    // Clear the line
+    for (i = 0; i < 30; i ++){
+      thisLine[i] = ' ';
+    }
+    
+    // Read from serial
+    Serial.readBytesUntil('\n', thisLine, 31);
+
+    //Print
+    printer.println(thisLine);
+    Serial.println(thisLine);
+  }
+}
--- server.py
+++
@@ -1,7 +0,0 @@
-from escpos import *
-
-DEVICE = ''
-
-p = printer.Serial(DEVICE)
-p.text('ALL YOUR BASE ARE BELONG TO US.')
-p.cut()
